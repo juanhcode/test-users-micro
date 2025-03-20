4cd819fce06f4a1eba896998afe399bf730ba0d2
Juan Hoyos <juanhoyos1347@gmail.com>
2025-03-20 15:55:41 -0500

    feat: delete all v2

--- src/test/java/com/develop/users_microservice/UsersMicroserviceApplicationTests.java
+++ src/test/java/com/develop/users_microservice/UsersMicroserviceApplicationTests.java
@@ -1,18 +1,12 @@
 package com.develop.users_microservice;
 
-import com.develop.users_microservice.domain.model.Role;
-import com.develop.users_microservice.domain.model.User;
-import com.develop.users_microservice.infrastructure.repository.UserRepositoryImpl;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = UsersMicroserviceApplication.class)
 class UsersMicroserviceApplicationTests {
-	@Autowired
-	private UserRepositoryImpl userRepository;
+
 
 	@Test
 	void contextLoads() {
